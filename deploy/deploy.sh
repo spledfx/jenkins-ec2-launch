78d010fb9ff8d3f821d4ae49d12f83686c0e0c6a
spledfx <saptorshi.pal@edifixio.com>
2021-08-20 11:14:59 +0530

    moved the script to a folder

---
+++ deploy/deploy.sh
@@ -0,0 +1,1 @@
+aws ec2 run-instances --image-id ami-02b4e72b17337d6c1 --count 1 --instance-type t2.micro --key-name jenkins-test --security-group-ids sg-019fd6f9d5bb17755 --subnet-id subnet-cb91f9af
